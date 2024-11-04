bde1c04c7416403d579104cd3265aacdc804c473
Willi Hübner <huebner@th-brandenburg.de>
2024-11-04 21:36:24 +0100

    made changes to API-Request

--- bk-a_scraper.py
+++ bk-a_scraper.py
@@ -1,15 +1,20 @@
 import logging
 import sqlite3
-import urllib
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-import bs4
+import requests
 
 #Add dnslookups from dns records
 #Add ip location of the webserver
 
-url = "https://bundes-klinik-atlas.de/krankenhaussuche/?searchtype=free-search"
+url = "https://bundes-klinik-atlas.de/searchresults/"
+params = {
+    "tx_solr[start]": 0,
+    "tx_solr[rows]": 20,
+    "searchtype": "free-search",
+    "tx_solr[sort]": "header,desc"
+}
+headers = {
+    "Accept": "application/json"
+}
 
 #SQLite Connection
 conn = sqlite3.connect("bk-a.db")
@@ -23,27 +28,11 @@ logging.basicConfig(filename='./Logs/bk-a.log', level=logging.INFO,
 
 logger.info("<-- Script started -->")
 
-def has_next_page(soup):
-    return soup.find('a', string='next') is not None
+def get_all_hospitals():
+    response = requests.get(url, params=params, headers=headers)
 
-def get_all_hospitals(url):
-    try:
-        logger.info(f"Requesting {url}")
-        driver = webdriver.Chrome()
-        driver.get(url)
-        driver.find_element("id", "free-search").click()
-        driver.implicitly_wait(1)
-        driver.find_element(By.CLASS_NAME, "ce-search-result-list-item")
-        html = driver.page_source
-        soup = bs4.BeautifulSoup(html, 'html.parser')
-        hospitals = soup.find_all('a', {'class': 'c-link'})
-        print(hospitals)
-
-
-        response = urllib.request.urlopen(url)
-        html = response.read().decode("utf-8")
-        soup = bs4.BeautifulSoup(html, 'html.parser')
-    except Exception as e:
-        logger.error(f"Script failed for {url}\n", exc_info=True)
-
-get_all_hospitals(url)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+    else:
+        print(f"Request failed with status code {response.status_code}")
